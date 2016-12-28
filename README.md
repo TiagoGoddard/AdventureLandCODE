58f75d4f4386ca7e02dae55320ac4e3589e07a97
Tiago Goddard <tiago@goddard.com.br>
2016-12-28 21:46:41 -0200

    Fix

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ My complete AdventureLandCODE
 
 ## Inside Game CODE Javascript:
 ```javascript
-var version = '0.37';
+var version = '0.38';
 
 // Handle party
 var party_leader = 'Washer';
--- upgrades.js
+++ upgrades.js
@@ -45,8 +45,8 @@ define(["require", "scripts/utils"],function (require, utils) {
 						let [item3_slot, item3] = utils.get_item_slot((item) => c.name === item.name && c.level === item.level, item2_slot + 1);
 						if (item2 && item3) {
 							parent.compoundit(i.name, c.level);
+							return;
 						}
-						return;
 					} else if (c && ewhitelist.includes(c.name)) {
 						//Exchange
 						let [item_slot, item] = utils.get_item_slot(i => i.name == c.name);
